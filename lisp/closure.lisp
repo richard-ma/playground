020eead33d33982170138407d4474b9f0adb4501
Richard Ma <richard.ma.19850509@gmail.com>
2012-12-26 16:13:22 +0800

    update

---
+++ lisp/closure.lisp
@@ -0,0 +1,7 @@
+(defparameter *fn* (let ((count 0)) #'(lambda () (setf count (1+ count)))))
+
+(let ((count 0))
+  (list
+   #'(lambda () (incf count))
+   #'(lambda () (decf count))
+   #'(lambda () count)))
