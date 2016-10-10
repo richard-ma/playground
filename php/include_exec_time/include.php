d7e50f063015077068e8212253cfdfc02c7bacbb
Richard Ma <richard.ma.19850509@gmail.com>
2016-10-10 14:36:23 +0800

    added

---
+++ php/include_exec_time/include.php
@@ -0,0 +1,5 @@
+<?php
+
+print "Start include file"."\n";
+sleep(3);
+print "End include file"."\n";
---
+++ php/include_exec_time/normal.php
@@ -0,0 +1,5 @@
+<?php
+
+print "Start normal file"."\n";
+include("./include.php");
+print "End normal file"."\n";
