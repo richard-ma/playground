624ea2957049fc3085763be56c0ab30eb1f108a7
Richard Ma <richard.ma.19850509@gmail.com>
2016-10-13 22:28:19 +0800

    add shopify_crewler

---
+++ python/shopify_crawler/shopify_crawler.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import requests
+import re
+
+# data format
+# [ {'site':'www.domain.com', 'status':'C'}
+#   {'site':'www.domain2.com', 'status':'S'} ]
+# C = completed
+# S = seed
+# new domain -> Seed -> Completed
+
+def main(argv):
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv)
